4fe4ce4a0085d20ff39ca708674454ec1f433875
Carlos Polanco <carlos.polanco0508@gmail.com>
2023-03-17 11:19:01 -0400

    added fonts sizes

---
+++ reactspringcomponents/src/components/Fonts/FontsStyle.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../../index.css';
+export default function FontsStyle() {
+  return (
+    <div>
+        <h1>Heading H1 34 Bold</h1>
+        <h2>Heading H2 28 Bold</h2>
+        <h2>Heading H3 24 Bold</h2>
+        <p className='Intro'>Body Intro 20 Medium</p>
+        <p> Body p  17 Regular</p>
+        <p className='caption'> .caption 15 Regular</p>
+        <p className='smallText'> .smallText 13 Regular</p>
+    </div>
+  )
+}
--- reactspringcomponents/src/components/PageLayout/Page/Page.tsx
+++ reactspringcomponents/src/components/PageLayout/Page/Page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Wrapper from '../Wrapper/Wrapper'
 import Content from '../Content/Content'
 import Hero from '../../Hero/Hero'
+import "../../../index.css"
 //@ts-ignore
 export default function Page() {
   return (
