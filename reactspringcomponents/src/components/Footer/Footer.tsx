c3b0c13f400142a78e2e729f183f1ec0139950c1
Carlos Polanco <carlos.polanco0508@gmail.com>
2023-03-29 19:36:04 -0400

    added checkbox, footer, and section components

---
+++ reactspringcomponents/src/components/Footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./Footer.module.css"
+import FooterItem from './FooterItem'
+export default function Footer() {
+  return (
+    <div className={styles.Wrapper}>
+        <div className={styles.Content}>
+            <h3>SmartCommerce</h3>
+           <FooterItem title={'Features'} listItem={['Men', 'Women', 'Boys', 'Girls', 'New Arrivals', 'Shoes', 'Accessories']} />
+           <FooterItem title={'Features'} listItem={['Men', 'Women', 'Boys', 'Girls', 'New Arrivals', 'Shoes', 'Accessories']} />
+           <FooterItem title={'Features'} listItem={['Men', 'Women', 'Boys', 'Girls', 'New Arrivals', 'Shoes', 'Accessories']} />
+           <FooterItem title={'Features'} listItem={['Men', 'Women', 'Boys', 'Girls', 'New Arrivals', 'Shoes', 'Accessories']} />
+        </div>
+    </div>
+  )
+}
